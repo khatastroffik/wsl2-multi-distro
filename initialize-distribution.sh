3c51554acea7fbdd8c99acc1061e56b30ce98339
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2021-05-24 12:56:42 +0200

    docs: improve script, add license and package information

--- initialize-distribution.sh
+++ initialize-distribution.sh
@@ -11,3 +11,6 @@ echo -e "\n===== 4 ===== Configure the screen displayed on start ========="
 sudo echo -e "cd ~\nclear\necho -e \"\033[1;92m\$(printf \"%0.s=\" {1..40})\\\n \$WSL_DISTRO_NAME - \$(lsb_release -d | grep -oP \"Description:\s+\K(.+)\") \\\n\$(printf \"%0.s=\" {1..40})\033[m\\\n\"" >> /home/$newuser/.profile
 echo "\n===== 5 ===== Update the distro and upgrade, clean ... ========"
 sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove && sudo apt -y autoclean
+echo -e "\n==============================================================="
+echo -e "\nSetup completed. Please exit, terminate and restart the distro"
+echo -e "\n==============================================================="
